a43b9c5fb7e1c30e67ea03b51291003afce480de
Julien <jbreuil@gmail.com>
2020-08-14 14:58:32 +0200

    add github link

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,8 +34,10 @@
         <Nuxt />
       </div>
     </main>
-    <footer class="w-full text-xs text-gray-300 bg-gray-800 border-grey p-4">
-      <div class="text-center">
+    <footer
+      class="flex flex-col w-full text-center text-gray-300 bg-gray-800 border-grey p-4"
+    >
+      <div class="text-xs">
         The name Valorant and all the images used on this website are the
         property of
         <a class="underline" href="https://www.riotgames.com/">Riot Games</a>.
@@ -43,6 +45,11 @@
         <a class="underline" href="https://www.riotgames.com/en/legal"
           >legal rules</a
         >.
+      </div>
+      <div class="mt-2">
+        <a href="https://github.com/jbreuil/valorant-scrambler">
+          <font-awesome-icon :icon="['fab', 'github']" size="lg" />
+        </a>
       </div>
     </footer>
   </div>
